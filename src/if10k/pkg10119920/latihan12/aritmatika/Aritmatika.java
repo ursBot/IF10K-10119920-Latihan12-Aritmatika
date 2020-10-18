375e6e0126dfcc509af022c0154e976139646822
ursBot <umar.10119920@mahasiswa.ac.id>
2020-10-18 17:19:47 +0700

    upload pertama

---
+++ src/if10k/pkg10119920/latihan12/aritmatika/Aritmatika.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119920.latihan12.aritmatika;
+
+
+/**
+*
+ * @author
+ * NAMA     : Umar Said Adi Pranoto
+ * KELAS    : IF10K
+ * NIM      : 10119920
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+
+public class Aritmatika{
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+
+
+}
